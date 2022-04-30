24fcfe1ac5d017270ce619867b42480bcff8ee0d
luizpcardoso <luizpauloreiscardosoo@gmail.com>
2022-04-30 14:05:10 -0300

    feat: create user route retrieve controller

--- app/controllers/user_controller.py
+++ app/controllers/user_controller.py
@@ -1,18 +1,37 @@
+from http import HTTPStatus
+
+from app.configs.database import db
+from app.models.user_model import UserModel
+from flask import jsonify
+from sqlalchemy.orm import Query
+from sqlalchemy.orm.session import Session
+
+
 def signup():
     ...
-
-
 def signin():
     ...
-
 #daqui pra baixo tem q ser autenticado
-def get_user():
+def retrieve():
+
+    session: Session = db.session
+    
+    base_query: Query = session.query(UserModel)
+
+    users = base_query.all()
+
+    return jsonify([{
+        "name": user.name,
+        "email":user.email,
+        "phone":user.phone,
+        "address":user.address.cep,
+        "city":user.address.cities.name,
+        "state":user.address.cities.state.name
+
+    } for user in users]), HTTPStatus.OK
+
+
+def delete():
     ...
-
-
-def delete_user():
-    ...
-
-
-def patch_user():
+def patch():
     ...
