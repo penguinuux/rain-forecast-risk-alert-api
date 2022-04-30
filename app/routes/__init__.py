1538e330e0c645796d779537fd41884f113e2578
sidny <sidny.silva@outlook.com>
2022-04-29 21:00:47 -0300

    feature: routes definition

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,8 +1,16 @@
-from flask import Blueprint, Flask
-
-bp_api = Blueprint("bp_api", __name__)
-
+from flask import Flask
 
 def init_app(app: Flask):
 
-    app.register_blueprint(bp_api)
+    from .address_blueprint import bp_address 
+    app.register_blueprint(bp_address)
+
+    from .message_blueprint import bp_message
+    app.register_blueprint(bp_message)
+
+    from .risk_blueprint import bp_risk
+    app.register_blueprint(bp_risk)
+
+    from .user_blueprint import bp_users
+    app.register_blueprint(bp_users)
+
