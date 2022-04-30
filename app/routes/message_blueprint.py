9572e494ed3fe84d0c43519a0597616ae5f26e11
Pablo A. Maximo <pabloadrianomc@gmail.com>
2022-04-30 17:35:57 -0300

    feat: remove "/" url prefix from routes

--- app/routes/message_blueprint.py
+++ app/routes/message_blueprint.py
@@ -4,4 +4,4 @@ from app.controllers.message_controller import messages
 
 bp_message = Blueprint("bp_message", __name__, url_prefix="/messages")
 
-bp_message.get("/")(messages)
+bp_message.get("")(messages)
