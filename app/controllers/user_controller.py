d480fccacd07db8337dfa02873da50a57d0884a9
luizpcardoso <luizpauloreiscardosoo@gmail.com>
2022-05-01 18:19:33 -0300

    fix: applied black and isort organization.

--- app/controllers/user_controller.py
+++ app/controllers/user_controller.py
@@ -1,37 +1,51 @@
 from http import HTTPStatus
 
-from app.configs.database import db
-from app.models.user_model import UserModel
 from flask import jsonify
 from sqlalchemy.orm import Query
 from sqlalchemy.orm.session import Session
 
+from app.configs.database import db
+from app.models.user_model import UserModel
+
 
 def signup():
     ...
+
+
 def signin():
     ...
-#daqui pra baixo tem q ser autenticado
+
+
+# daqui pra baixo tem q ser autenticado
 def retrieve():
 
     session: Session = db.session
-    
+
     base_query: Query = session.query(UserModel)
 
     users = base_query.all()
 
-    return jsonify([{
-        "name": user.name,
-        "email":user.email,
-        "phone":user.phone,
-        "address":user.address.cep,
-        "city":user.address.cities.name,
-        "state":user.address.cities.state.name
-
-    } for user in users]), HTTPStatus.OK
+    return (
+        jsonify(
+            [
+                {
+                    "name": user.name,
+                    "email": user.email,
+                    "phone": user.phone,
+                    "address": user.address.cep,
+                    "city": user.address.cities.name,
+                    "state": user.address.cities.state.name,
+                }
+                for user in users
+            ]
+        ),
+        HTTPStatus.OK,
+    )
 
 
 def delete():
     ...
+
+
 def patch():
     ...
--- app/routes/risk_blueprint.py
+++ app/routes/risk_blueprint.py
@@ -1,7 +1,7 @@
 from flask import Blueprint
+
 from app.controllers.risk_controller import risks
 
+bp_risk = Blueprint("bp_risk", __name__, url_prefix="/risk")
 
-bp_risk = Blueprint('bp_risk',__name__,url_prefix='/risk')
-
-bp_risk.get('/')(risks)
+bp_risk.get("/")(risks)
