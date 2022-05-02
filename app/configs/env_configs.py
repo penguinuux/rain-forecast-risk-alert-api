f26c21b840d578d04cea2d8d57936484cf94fdb4
sidny <sidny.silva@outlook.com>
2022-05-02 16:03:21 -0300

    fix: instance JTWManager
    created init_app to authenticate JWT configs

--- app/configs/env_configs.py
+++ app/configs/env_configs.py
@@ -8,3 +8,4 @@ def init_app(app: Flask):
     app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URI")
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     app.config["JSON_SORT_KEYS"] = False
+    app.config["JWT_SECRET_KEY"] = getenv("SECRET")
