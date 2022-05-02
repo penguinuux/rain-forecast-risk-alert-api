32c8121febea08a17c24a32eb6bc7e83e41ebfa6
Pablo A. Maximo <pabloadrianomc@gmail.com>
2022-05-02 20:04:30 -0300

    fix: remove blueprint duplicate url prefix

--- app/routes/forecast_risk_blueprint.py
+++ app/routes/forecast_risk_blueprint.py
@@ -4,4 +4,4 @@ from app.controllers import forecast_risk_controller
 
 bp = Blueprint("bp_forecast_risk", __name__, "/forecast-risk")
 
-bp.post("/forecast-risk")(forecast_risk_controller.fetch_forecast_risk)
+bp.post("")(forecast_risk_controller.fetch_forecast_risk)
