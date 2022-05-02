3932e5f34330a72ef4a52e97f2a2c9eff059229c
sidny <sidny.silva@outlook.com>
2022-05-02 16:03:21 -0300

    fix: instance JTWManager
    created init_app to authenticate JWT configs

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 
 from app import routes
-from app.configs import database, env_configs, migrations
+from app.configs import auth, database, env_configs, migrations
 
 
 def create_app():
@@ -11,5 +11,5 @@ def create_app():
     database.init_app(app)
     migrations.init_app(app)
     routes.init_app(app)
-
+    auth.init_app(app)
     return app
