37351ad8d19972d4983bb33b0d485c4d914c02ea
luizpcardoso <luizpauloreiscardosoo@gmail.com>
2022-05-02 20:48:35 -0300

    feat: create retrieve endpoint in message routes

--- app/controllers/message_controller.py
+++ app/controllers/message_controller.py
@@ -1,2 +1,32 @@
-def messages():
-    ...
+from http import HTTPStatus
+
+from flask import jsonify
+from sqlalchemy.orm import Query, Session
+
+from app.configs.database import db
+from app.models.message_model import MessageModel
+
+
+def retrieve():
+
+    session: Session = db.session
+
+    base_query: Query = session.query(MessageModel)
+
+    messages = base_query.all()
+
+    return (
+        jsonify(
+            [
+                {
+                    "message id": message.id,
+                    "title": message.title,
+                    "text": message.text,
+                    "date": message.date,
+                    "related users": message.users,
+                }
+                for message in messages
+            ]
+        ),
+        HTTPStatus.OK,
+    )
