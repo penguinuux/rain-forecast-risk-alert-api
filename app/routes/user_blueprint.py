74e48e57f8ffed2de11f454ff81fa18ac998867a
Marília Calado <mgcd.calado@gmail.com>
2022-05-03 19:35:10 -0300

    feat: post and patch user risk profile

--- app/routes/user_blueprint.py
+++ app/routes/user_blueprint.py
@@ -1,6 +1,6 @@
 from flask import Blueprint
 
-from app.controllers import user_controller
+from app.controllers import user_controller, user_risk_controller
 
 bp_users = Blueprint("bp_users", __name__, url_prefix="/user")
 
@@ -9,3 +9,4 @@ bp_users.post("/signin")(user_controller.signin)
 bp_users.get("")(user_controller.retrieve)
 bp_users.patch("")(user_controller.patch)
 bp_users.delete("")(user_controller.delete)
+bp_users.post("/risk-profile")(user_risk_controller.create_user_risk_profile)
