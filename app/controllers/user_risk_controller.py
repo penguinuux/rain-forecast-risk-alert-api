1052f78003bf88c6b4f7b8e1454f61bec36c8d0a
Marília Calado <mgcd.calado@gmail.com>
2022-05-03 19:35:10 -0300

    feat: post and patch user risk profile

---
+++ app/controllers/user_risk_controller.py
@@ -0,0 +1,61 @@
+from http import HTTPStatus
+
+from flask import request
+from flask_jwt_extended import decode_token, jwt_required
+from sqlalchemy.orm import Query, Session
+
+from app.configs.database import db
+from app.exceptions.user_exc import UserNotFound
+from app.models.risk_model import RiskModel
+from app.models.user_model import UserModel
+
+
+@jwt_required()
+def create_user_risk_profile():
+    data = request.get_json()
+    session: Session = db.session
+    token = request.headers.get("Authorization").split()[-1]
+    decoded_jwt = decode_token(token)
+    user_id = decoded_jwt.get("sub")
+    user: UserModel = UserModel.query.get(user_id)
+
+    if not user:
+        raise UserNotFound
+
+    selected_case = ""
+
+    for case_key, case_value in RiskModel.CASES.items():
+        # TODO: find a better name to count variable
+        count = 0
+        for key, value in data.items():
+            if value == case_value[key]:
+                count += 1
+            if count == 2:
+                selected_case = case_key
+
+    message = RiskModel.VALUES[selected_case]
+    risk_query: Query = session.query(RiskModel)
+
+    risk: RiskModel = risk_query.filter_by(title=selected_case).first()
+
+    if not risk:
+
+        risk_data = {"title": selected_case, "text": message}
+
+        risk = RiskModel(**risk_data)
+
+    if user.risks:
+        user.risks = []
+
+    risk.users.append(user)
+    session.add(risk)
+    session.commit()
+
+    serialized_user = {
+        "name": user.name,
+        "phone": user.phone,
+        "email": user.email,
+        "risks": user.risks,
+    }
+
+    return serialized_user, HTTPStatus.CREATED
--- app/models/risk_model.py
+++ app/models/risk_model.py
@@ -8,7 +8,6 @@ from app.configs.database import db
 @dataclass
 class RiskModel(db.Model):
     title: str
-    description: str
     text: str
 
     # Rain precipitation limit in mm
@@ -18,5 +17,34 @@ class RiskModel(db.Model):
 
     id = Column(Integer, primary_key=True)
     title = Column(String, nullable=False, unique=True)
-    description = Column(String, nullable=False)
     text = Column(String, nullable=False)
+
+    CASES = {
+        "NOT_SPECIFIED": {"live_nearby_river": False, "live_nearby_mountain": False},
+        "EARTH_SLIDING": {"live_nearby_river": False, "live_nearby_mountain": True},
+        "FLOOD": {"live_nearby_river": True, "live_nearby_mountain": False},
+        "FLOOD_AND_SLIDING": {"live_nearby_river": True, "live_nearby_mountain": True},
+    }
+
+    # TODO: find a better way to display the texts
+    VALUES = {
+        "NOT_SPECIFIED": """Atenção! A sua região apresenta previsão para chuvas fortes (ou violentas <depende da milimetragem>). 
+                   
+                   Se você mora em região de encostas ou próximo a um rio ou córrego, reúna seus pertences mais importantes 
+                   (documentos, dinheiro ou cartões) e se encaminhe para o ponto de apoio da Defesa Civil mais próximo.""",
+        "EARTH_SLIDING": """Atenção! A sua região apresenta previsão para chuvas fortes (ou violentas <depende da milimetragem>). 
+                   Você corre risco iminente de deslizamento de terra e possível desabamento. 
+
+                   Se ainda não começou a chover, reúna seus pertences mais importantes (documentos, dinheiro ou cartões) e 
+                   se encaminhe para o ponto de apoio da Defesa Civil mais próximo.""",
+        "FLOOD": """Atenção! A sua região apresenta previsão para chuvas fortes (ou violentas <depende da milimetragem>). 
+                   Você corre risco iminente de deslizamento de terra e possível desabamento. 
+
+                   Se ainda não começou a chover, reúna seus pertences mais importantes (documentos, dinheiro ou cartões) e 
+                   se encaminhe para o ponto de apoio da Defesa Civil mais próximo.""",
+        "FLOOD_AND_SLIDING": """Atenção! A sua região apresenta previsão para chuvas fortes (ou violentas <depende da milimetragem>). 
+                   Você corre risco iminente de deslizamento de terra, desabamento e alagamento. 
+
+                   Se ainda não começou a chover, reúna seus pertences mais importantes (documentos, dinheiro ou cartões) e 
+                   se encaminhe para o ponto de apoio da Defesa Civil mais próximo.""",
+    }
