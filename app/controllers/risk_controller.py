a4f6e5bc57aec8ef154a483681c1a26c5b339804
sidny <sidny.silva@outlook.com>
2022-05-03 20:35:54 -0300

    feat: risk controller
    function created to get all risks registered at database

--- app/controllers/risk_controller.py
+++ app/controllers/risk_controller.py
@@ -1,2 +1,15 @@
+from http import HTTPStatus
+
+from flask import jsonify
+from sqlalchemy.orm import Session
+
+from app.configs.database import db
+from app.models.risk_model import RiskModel
+
 def risks():
-    ...
+    
+    session: Session = db.session
+
+    risks =  session.query(RiskModel).all()
+
+    return jsonify(risks),HTTPStatus.OK
