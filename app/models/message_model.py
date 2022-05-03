773cc2c5e5a3df0746c15c416885c48f8da6d0f8
Pablo A. Maximo <pabloadrianomc@gmail.com>
2022-05-03 20:56:47 -0300

    feat: change MessageModel columns
    Removed columns title and text from "messages" table;
    Set column date a default value.

--- app/models/message_model.py
+++ app/models/message_model.py
@@ -1,19 +1,14 @@
 from dataclasses import dataclass
+from datetime import datetime
 
-from sqlalchemy import Column, DateTime, Integer, String
+from sqlalchemy import Column, DateTime, Integer
 
 from app.configs.database import db
 
 
 @dataclass
 class MessageModel(db.Model):
-    title: str
-    text: str
-    date: str
-
     __tablename__ = "messages"
 
     id = Column(Integer, primary_key=True)
-    title = Column(String, nullable=False)
-    text = Column(String, nullable=False)
-    date = Column(DateTime, nullable=False)
+    date = Column(DateTime, nullable=False, default=datetime.now())
