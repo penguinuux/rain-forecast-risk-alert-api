a14a371b778a76e11ef66f931dba25ae9b079ba2
josue <jow17alves@gmail.com>
2022-05-03 10:15:37 -0300

    fix: change of return, imports and name of variables

--- app/routes/cities_blueprint.py
+++ app/routes/cities_blueprint.py
@@ -1,8 +1,8 @@
 from flask import Blueprint
 
-from app.controllers.cities_controller import cities, city
+from app.controllers import cities_controller
 
 bp = Blueprint("bp_cities", __name__, url_prefix="/cities")
 
-bp.get("")(cities)
-bp.get("/<state>")(city)
+bp.get("")(cities_controller.all_states_and_cities)
+bp.get("/from-state/<state>")(cities_controller.all_cities_from_state)
