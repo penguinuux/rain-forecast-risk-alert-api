140d409163e2229abf35464df4e296bc2e0db04f
sidny <sidny.silva@outlook.com>
2022-05-05 20:19:09 -0300

    fix: normalizer
    user name and email normalized

--- app/controllers/user_controller.py
+++ app/controllers/user_controller.py
@@ -24,6 +24,7 @@ from app.exceptions.user_exc import UserNotFound
 from app.models.address_model import AddressModel
 from app.models.user_model import UserModel
 from app.services.generic_services import get_user_from_token
+from app.services.user_data_formater_services import validate_data
 from app.services.user_risk_profile_services import insert_default_risk
 from app.services.user_services import validate_keys_and_values
 from app.utils.zip_code_validate import validate_zip_code
@@ -36,6 +37,8 @@ def signup():
     try:
 
         validate_keys_and_values(data, signup=True)
+
+        validate_data(data)
 
         cep = data.pop("cep")
         city_query = asyncio.run(validate_zip_code(cep))
