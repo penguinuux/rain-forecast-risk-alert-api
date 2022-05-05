c072ffee46b0452911f2fcb19c3842da4d0852d3
sidny <sidny.silva@outlook.com>
2022-05-05 20:33:38 -0300

    feat: user patch normalize
    all data normalized in patch users.

--- app/controllers/user_controller.py
+++ app/controllers/user_controller.py
@@ -159,6 +159,7 @@ def patch():
         data = request.get_json()
         user = get_user_from_token()
 
+        validate_data(data)
         validate_keys_and_values(data, user, update=True)
 
         cep = data.get("cep", None)
