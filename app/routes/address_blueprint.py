abde40e454c91df53cf6832f31cbbf2a60282aa4
Pablo A. Maximo <pabloadrianomc@gmail.com>
2022-05-05 21:13:17 -0300

    feat: remove deprecated routes
    Removed post risk, get risk, get users, get address endpoints.

--- app/routes/address_blueprint.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint
-
-from app.controllers.address_controller import address
-
-bp_address = Blueprint("bp_address", __name__, url_prefix="/address")
-
-bp_address.get("")(address)
